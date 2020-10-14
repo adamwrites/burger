1380c4bd30c189a9486f6c23a73da6f219bc3af6
Adam Tatar <atatar2019@gmail.com>
2020-10-14 12:07:38 -0400

    heroku is annoying

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express = require("express");
+
+var app = express();
+app.use(express.static(process.cwd() + '/public'));
+
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var router = require('./controllers/burgers_controller.js');
+app.use('/', router);
+
+var port = process.env.PORT || 3000;
+app.listen(port);
